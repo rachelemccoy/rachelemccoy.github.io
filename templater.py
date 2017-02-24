dd4e2459491a09eff051018041c2f9341aa2e9f6
Elliot Lockerman <elliot@lockerman.info>
2017-02-23 21:46:05 -0500

    Added per-page title and css to templater, tweaked column widths, imports for project page css

--- templater.py
+++ templater.py
@@ -2,8 +2,15 @@
 
 import os
 import sys
+import copy
+
+DEFAULT_TITLE = "Rachel E. McCoy"
 
 def template(root):
+
+    with open("components/head.html") as f:
+        head = ["<!DOCTYPE html>", "<head>"] + f.readlines()
+
     with open("components/header.html") as f:
         header = f.readlines()
 
@@ -19,7 +26,26 @@ def template(root):
                 with open(inpath) as f:
                     contents = f.readlines()
 
-                data = header + contents + footer
+                page_head = copy.copy(head)
+                title_path = os.path.join(dirname, file.replace("-template", "-title"))
+                if os.path.exists(title_path):
+                    with open(title_path) as f:
+                        title = f.readline()    
+                else:
+                    title = DEFAULT_TITLE
+
+                page_head.append("<title>" + title + "</title>")
+
+
+                css_file = file.replace("-template.html", ".css")
+                css_path = os.path.join(dirname, css_file)
+                if(os.path.exists(css_path)):
+                    page_head.append("<link rel=\"stylesheet\" href=\"" + css_file + "\">")
+
+                page_head.append("</head>")
+
+
+                data = page_head + header + contents + footer
 
                 outpath = os.path.join(dirname, file.replace("-template", ""))
                 with open(outpath, "w") as f:
